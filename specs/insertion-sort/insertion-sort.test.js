59f2923f3e490b1228a9976ccdb47fffc8eb25e1
Ebraheem <ebraheemsaid7@gmail.com>
2025-04-13 23:00:17 +0200

    insertion-sort done

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -16,14 +16,14 @@
 function insertionSort(nums) {
   // code goes here
   for (let i = 1; i < nums.length; i++) {
-    let current = nums[i];
+    let temp = nums[i];
     let j = i - 1;
-    // snapshot(nums); // Uncomment this line to visualize the sorting process
-    while (j >= 0 && nums[j] > current) {
+
+    while (j >= 0 && nums[j] > temp) {
       nums[j + 1] = nums[j];
       j--;
     }
-    nums[j + 1] = current;
+    nums[j + 1] = temp;
   }
   return nums;
 }
